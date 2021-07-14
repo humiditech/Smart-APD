b15f05ac099994ddbeafac7f722d0ebd946f8cbd
aryawiratama18 <NAwiratama@gmail.com>
2021-07-15 06:45:46 +0700

    Webserver worked

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include <DHT.h>
+#include "DHT.h"
 #include "FS.h"
 #include "SPIFFS.h"
 #include <SPI.h>
@@ -12,52 +13,43 @@ String version = "v1.0";
 String site_width = "1023";
 WiFiClient client;
 AsyncWebServer server(80);
+AsyncEventSource events("/events");
 
-int log_time_unit = 15;
-int time_reference = 60;
-int const table_size = 72;
-int index_ptr, timer_cnt, log_interval, log_count, max_temp, min_temp;
-String webpage, time_now, log_time, lastcall, time_str, DataFile = "datalog.txt";
-bool AScale, auto_smooth, AUpdate, log_delete_approved;
-float temp, humi;
-
-const long gmtOffset_sec = 25200; //GMT +7.00 : 7 *60 *60
-const long daylightOffset_sec = 0;
-const char *ntpServer = "pool.ntp.org";
-
-// typedef signed short sint16;
-typedef struct
-{
-  int lcnt;
-  String ltime;
-  float temp;
-  float humi;
-} record_type;
-
-record_type sensor_data[table_size + 1];
-
+// DHT 22
 #define DHT_PIN 34
 #define DHT_TYPE DHT11
 
 DHT dht(DHT_PIN, DHT_TYPE);
-unsigned long current_time = 0;
-const int interval = 1000;
+// unsigned long current_time = 0;
+// const int interval = 1000;
+
+// Flow Sensor
+#define FLOW_PIN 16
 
 // Relay
 #define KIPAS 25
 #define UV 26
 #define PELTIER 27
 
+// Variables
+float temp, humi;
+long currentMillis = 0;
+long previousMillis = 0;
+int interval = 1000;
+float calibrationFactor = 4.5;
+volatile byte pulseCount;
+byte pulse1Sec = 0;
+float flowRate;
+unsigned int flowMiliLitres;
+unsigned int totalMiliLitres;
+
 // Functions declaration
 void StartSPIFFS();
 int StartWiFi(const char *ssid, const char *password);
-void StartTime();
-void UpdateLocalTime();
-String GetTime();
-String calcDateTime(int epoch);
-void reset_array();
-String readDHTTemp();
-String readDHTHumidity();
+void IRAM_ATTR pulseCounter();
+void readDHTSensors();
+void readFlowSensors();
+String processor(const String &var);
 
 void setup()
 {
@@ -66,110 +58,92 @@ void setup()
   pinMode(UV, OUTPUT);
   pinMode(PELTIER, OUTPUT);
   pinMode(DHT_PIN, INPUT);
+  pinMode(FLOW_PIN, INPUT_PULLUP);
   digitalWrite(KIPAS, HIGH);
   digitalWrite(UV, HIGH);
   digitalWrite(PELTIER, HIGH);
 
+  pulseCount = 0;
+  flowRate = 0.0;
+  flowMiliLitres = 0;
+  totalMiliLitres = 0;
+  previousMillis = 0;
+
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
   dht.begin();
   // StartSPIFFS();
   StartWiFi(ssid, password);
-  // StartTime();
-  // Serial.println(F("WiFi Connected ...."));
-  // Serial.println(F("Webserver started ..."));
-  // Serial.println("Connect to this url : http://" + WiFi.localIP().toString() + "/");
+  attachInterrupt(digitalPinToInterrupt(FLOW_PIN), pulseCounter, FALLING);
 
-  // server.begin();
-  // server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-  //           { request->send(SPIFFS, "/index.html"); });
-
-  // server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request)
-  //           { request->send_P(200, "text/plain", readDHTTemp().c_str()); });
-
-  // server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request)
-  //           { request->send_P(200, "text/plain", readDHTHumidity().c_str()); });
-
-  // index_ptr = 0;  // Array pointer
-  // log_count = 0;  // Data logger counter
-  // max_temp = 40;  // Maximum displayed temperature
-  // min_temp = -10; // Minimum displayed temperature
-  // lastcall = "temp_humi";
-  // log_interval = log_time_unit * 10;
-  // timer_cnt = log_interval + 1;
-  // log_delete_approved = false;
-  // reset_array();
+  // Handle Web Server
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/index.html", String(), false, processor); });
+  // Handle Web Server Events
+  events.onConnect([](AsyncEventSourceClient *client)
+                   {
+                     if (client->lastId())
+                     {
+                       Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+                     }
+                     client->send("Hello!", NULL, millis(), 10000);
+                   });
+  server.addHandler(&events);
+  server.begin();
 }
 
 void loop()
 {
-  humi = dht.readHumidity();
-  temp = dht.readTemperature();
-
-  if (isnan(humi) || isnan(temp))
+  if ((millis() - previousMillis) > interval)
   {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
+    readDHTSensors();
+    readFlowSensors();
+
+    previousMillis = millis();
+    flowMiliLitres = (flowRate / 60) * 1000;
+    totalMiliLitres += flowMiliLitres;
+
+    Serial.print(F("Flow rate: "));
+    Serial.print(int(flowRate)); // Print the integer part of the variable
+    Serial.print(F(" L/min"));
+    Serial.print("\t"); // Print tab space
+
+    // Print the cumulative total of litres flowed since starting
+    Serial.print(F("Output Air Quantity: "));
+    Serial.print(totalMiliLitres);
+    Serial.print(F(" mL / "));
+    Serial.print(totalMiliLitres / 1000);
+    Serial.println(F(" L"));
+
+    events.send("ping", NULL, millis());
+    events.send(String(temp).c_str(), "temperature", millis());
+    events.send(String(humi).c_str(), "humidity", millis());
+    events.send(String(flowRate).c_str(), "flow_rate", millis());
   }
-
-  // time_t now = time(nullptr);
-  // time_now = String(ctime(&now)).substring(0, 24);
-
-  // if (time_now != "Thu Jan 01 00:00:00 1970" && timer_cnt >= log_interval)
-  // {
-  //   timer_cnt = 0;
-  //   log_count += 1;
-  //   sensor_data[index_ptr].lcnt = log_count;
-  //   sensor_data[index_ptr].temp = temp;
-  //   sensor_data[index_ptr].humi = humi;
-  //   sensor_data[index_ptr].ltime = calcDateTime(time(&now)); // Timestamp
-
-  //   File datafile = SPIFFS.open("/" + DataFile, FILE_APPEND);
-  //   time_t now = time(nullptr);
-  //   if (datafile == true)
-  //   {                                                                                                                                                                                   // if the file is available, write to it
-  //     datafile.println(((log_count < 10) ? "0" : "") + String(log_count) + char(9) + String(temp / 10, 2) + char(9) + String(humi / 10, 2) + char(9) + calcDateTime(time(&now)) + "."); // TAB delimited
-  //     Serial.println(((log_count < 10) ? "0" : "") + String(log_count) + " New Record Added");
-  //   }
-  //   datafile.close();
-  //   index_ptr += 1;
-  //   if (index_ptr > table_size)
-  //   {
-  //     index_ptr = table_size;
-  //     for (int i = 0; i < table_size; i++) // If data table full, scroll to the left and add new reading to the end
-  //     {
-  //       sensor_data[i].lcnt = sensor_data[i + 1].lcnt;
-  //       sensor_data[i].temp = sensor_data[i + 1].temp;
-  //       sensor_data[i].humi = sensor_data[i + 1].humi;
-  //       sensor_data[i].ltime = calcDateTime(time(&now));
-  //     }
-  //   }
-  //   timer_cnt += 1;
-  // }
-  Serial.print(F("Humidity: "));
-  Serial.print(humi);
-  Serial.print(F(" %   Temperature : "));
-  Serial.print(temp);
-  Serial.println(F(" °C"));
-  delay(2000);
 }
 
-void StartSPIFFS()
-{
-  boolean SPIFFS_Status;
-  SPIFFS_Status = SPIFFS.begin();
-  if (SPIFFS_Status == false)
-  { // Most likely SPIFFS has not yet been formated, so do so
-    SPIFFS.begin();
-    File datafile = SPIFFS.open("/" + DataFile, FILE_READ);
-    if (!datafile || !datafile.isDirectory())
-    {
-      Serial.println("SPIFFS failed to start..."); // If ESP32 nothing more can be done, so delete and then create another file
-      SPIFFS.remove("/" + DataFile);               // The file is corrupted!!
-      datafile.close();
-    }
-  }
-  else
-    Serial.println("SPIFFS Started successfully...");
-}
+// void StartSPIFFS()
+// {
+//   boolean SPIFFS_Status;
+//   SPIFFS_Status = SPIFFS.begin();
+//   if (SPIFFS_Status == false)
+//   { // Most likely SPIFFS has not yet been formated, so do so
+//     SPIFFS.begin();
+//     File datafile = SPIFFS.open("/" + DataFile, FILE_READ);
+//     if (!datafile || !datafile.isDirectory())
+//     {
+//       Serial.println("SPIFFS failed to start..."); // If ESP32 nothing more can be done, so delete and then create another file
+//       SPIFFS.remove("/" + DataFile);               // The file is corrupted!!
+//       datafile.close();
+//     }
+//   }
+//   else
+//     Serial.println("SPIFFS Started successfully...");
+// }
 
 int StartWiFi(const char *ssid, const char *password)
 {
@@ -193,129 +167,50 @@ int StartWiFi(const char *ssid, const char *password)
   return 1;
 }
 
-void StartTime()
+void IRAM_ATTR pulseCounter()
 {
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  UpdateLocalTime();
+  pulseCount++;
 }
 
-void UpdateLocalTime()
+void readDHTSensors()
 {
-  struct tm timeInfo;
-  while (!getLocalTime(&timeInfo))
+  humi = dht.readHumidity();
+  temp = dht.readTemperature();
+
+  if (isnan(humi) || isnan(temp))
   {
-    Serial.println("Failed to obtain time");
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
   }
-  Serial.println(&timeInfo, "%a %b %d %Y   %H:%M:%S");
-  char output[50];
-  strftime(output, 50, "%a %d-%b-%y  (%H:%M:%S)", &timeInfo);
-  time_str = output;
+
+  Serial.print(F("Humidity: "));
+  Serial.print(humi);
+  Serial.print(F(" %   Temperature : "));
+  Serial.print(temp);
+  Serial.println(F(" °C"));
 }
 
-String GetTime()
+void readFlowSensors()
 {
-  struct tm timeInfo;
-  while (!getLocalTime(&timeInfo))
-  {
-    Serial.println("Failed to obtain time - trying again");
-  }
-  Serial.println(&timeInfo, "%a %b %d %Y %H:%M:%S");
-  char output[50];
-  strftime(output, 50, "%d/%m/%y %H:%M:%S", &timeInfo);
-  time_str = output;
-  Serial.println(time_str);
-  return time_str;
+  pulse1Sec = pulseCount;
+  pulseCount = 0;
+  flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor;
 }
 
-String calcDateTime(int epoch)
-{ // From UNIX time becuase google charts can use UNIX time
-  int seconds, minutes, hours, dayOfWeek, current_day, current_month, current_year;
-  seconds = epoch;
-  minutes = seconds / 60;  // calculate minutes
-  seconds -= minutes * 60; // calculate seconds
-  hours = minutes / 60;    // calculate hours
-  minutes -= hours * 60;
-  current_day = hours / 24; // calculate days
-  hours -= current_day * 24;
-  current_year = 1970; // Unix time starts in 1970
-  dayOfWeek = 4;       // on a Thursday
-  while (1)
-  {
-    bool leapYear = (current_year % 4 == 0 && (current_year % 100 != 0 || current_year % 400 == 0));
-    uint16_t daysInYear = leapYear ? 366 : 365;
-    if (current_day >= daysInYear)
-    {
-      dayOfWeek += leapYear ? 2 : 1;
-      current_day -= daysInYear;
-      if (dayOfWeek >= 7)
-        dayOfWeek -= 7;
-      ++current_year;
-    }
-    else
-    {
-      dayOfWeek += current_day;
-      dayOfWeek %= 7;
-      /* calculate the month and day */
-      static const uint8_t daysInMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-      for (current_month = 0; current_month < 12; ++current_month)
-      {
-        uint8_t dim = daysInMonth[current_month];
-        if (current_month == 1 && leapYear)
-          ++dim; // add a day to February if a leap year
-        if (current_day >= dim)
-          current_day -= dim;
-        else
-          break;
-      }
-      break;
-    }
-  }
-  current_month += 1; // Months are 0..11 and returned format is dd/mm/ccyy hh:mm:ss
-  current_day += 1;
-  String date_time = (current_day < 10 ? "0" + String(current_day) : String(current_day)) + "/" + (current_month < 10 ? "0" + String(current_month) : String(current_month)) + "/" + String(current_year).substring(2) + " ";
-  date_time += ((hours < 10) ? "0" + String(hours) : String(hours)) + ":";
-  date_time += ((minutes < 10) ? "0" + String(minutes) : String(minutes)) + ":";
-  date_time += ((seconds < 10) ? "0" + String(seconds) : String(seconds));
-  return date_time;
-}
-
-void reset_array()
+String processor(const String &var)
 {
-  for (int i = 0; i <= table_size; i++)
+  readDHTSensors();
+  readFlowSensors();
+  if (var == "TEMPERATURE")
   {
-    sensor_data[i].lcnt = 0;
-    sensor_data[i].temp = 0;
-    sensor_data[i].humi = 0;
-    sensor_data[i].ltime = "";
-  }
-}
-
-String readDHTTemp()
-{
-  float temp = dht.readTemperature();
-  if (isnan(temp))
-  {
-    Serial.println("Failed to read temperature");
-    return "";
-  }
-  else
-  {
-    Serial.println(temp);
     return String(temp);
   }
-}
-
-String readDHTHumidity()
-{
-  float humid = dht.readHumidity();
-  if (isnan(humid))
+  else if (var == "HUMIDITY")
   {
-    Serial.println("Failed to read humidity");
-    return "";
+    return String(humi);
   }
-  else
+  else if (var == "FLOWRATE")
   {
-    Serial.println(humid);
-    return String(humid);
+    return String(flowRate);
   }
 }
