947692c1395d95835d8678a24928fdc5642f9cfe
aryawiratama18 <NAwiratama@gmail.com>
2021-07-18 16:17:46 +0700

    Change DHT PIN

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,29 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include "DHT.h"
 #include "FS.h"
 #include "SPIFFS.h"
 #include <SPI.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-// #include <WebServer.h>
 #include <ArduinoJson.h>
 #include <WebSocketsServer.h>
 #include <WiFiClientSecure.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 #include "credentials.h"
 
 String version = "v1.0";
 String site_width = "1023";
 WiFiClient client;
-// WebServer server(80);
 AsyncWebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
-// AsyncWebSocket ws("/ws");
 
 // DHT 22
-#define DHT_PIN 34
+#define DHT_PIN 23
 #define DHT_TYPE DHT11
 
-DHT dht(DHT_PIN, DHT_TYPE);
+DHT_Unified dht(DHT_PIN, DHT_TYPE);
 
 // Flow Sensor
 #define FLOW_PIN 16
@@ -54,188 +53,6 @@ const char *param1 = "tempThres";
 const char *param2 = "humidThres";
 const char *param3 = "flowThres";
 
-const char index_html[] PROGMEM = R"rawliteral(
-  <!DOCTYPE HTML>
-<html>
-
-<head>
-    <title>Smart APD </title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.2/css/all.css'
-        integrity='sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr' crossorigin='anonymous'>
-    <link rel='icon' href='data:,'>
-    <script src='https://code.highcharts.com/highcharts.js'></script>
-    <style>
-        html {
-            font-family: Arial, sans-serif;
-            display: inline-block;
-            text-align: center;
-        }
-
-        p {
-            font-size: 1.2rem;
-        }
-
-        body {
-            min-width: 310px;
-            max-width: 800px;
-            height: 400px;
-            margin: 0 auto;
-        }
-
-        .topnav {
-            overflow: hidden;
-            background-color: #50B8B4;
-            color: white;
-            font-size: 1rem;
-        }
-
-        .content {
-            padding: 20px;
-        }
-
-        .card {
-            background-color: white;
-            box-shadow: 2px 2px 12px 1px rgba(140, 140, 140, .5);
-            border-radius: 5px;
-        }
-
-        .cards {
-            max-width: 800px;
-            margin: 10px auto;
-            display: grid;
-            grid-gap: 2rem;
-            grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-        }
-
-        .reading {
-            font-size: 1.4rem;
-        }
-
-        @media screen and (max-width : 320px) {
-            .cards {
-                max-width: 100px;
-                margin: 10px auto;
-                grid-template-columns: repeat(3, 1fr);
-            }
-        }
-    </style>
-</head>
-
-<body>
-    <div class='topnav'>
-        <h1>Smart APD Webserver</h1>
-    </div>
-    <div class='content'>
-        <div class='cards'>
-            <div class='card'>
-                <p><i class='fas fa-thermometer-half' style='color:#059e8a;'></i> TEMPERATURE</p>
-                <p><span class='reading'><span id='temp'>%TEMPERATURE%</span> &deg;C</span></p>
-            </div>
-            <div class='card'>
-                <p><i class='fas fa-tint' style='color:#00add6;'></i> HUMIDITY</p>
-                <p><span class='reading'><span id='hum'>%HUMIDITY%</span> &percnt;</span></p>
-            </div>
-            <div class='card'>
-                <p><i class='fas fa-angle-double-down' style='color:#e1e437;'></i> FLOW RATE</p>
-                <p><span class='reading'><span id='flow'>%FLOWRATE%</span> L/min</span></p>
-            </div>
-        </div>
-
-        <div class='cards'>
-            <div class='card'>
-                <p>UV STATUS</p>
-                <p class='reading'><span id='uvstats'>%UVSTATUS%</span></p>
-            </div>
-            <div class='card'>
-                <p>FAN STATUS</p>
-                <p class='reading'><span id='fanstats'>%FANSTATUS%</span></p>
-            </div>
-            <div class='card'>
-                <p>COOLER STATUS</p>
-                <p class='reading'><span id='tcstats'>%THERMOCOOLERSTATUS%</span></p>
-            </div>
-        </div>
-
-        <form action='/get' target='hidden-form'>
-            <p>Temperature Threshold : (current value %tempThres%)</p>
-            <div class='inputField'>
-              <input type='text' name='tempThres'>
-              <input type='submit' value='Submit' onClick='submitMessage()'>
-            </div>
-        </form><br>
-        <form action='/get' target='hidden-form'>
-            <p>Humidity Threshold : (current value %humidThres%)</p>
-            <div class='inputField'>
-              <input type='text' name='humidThres'>
-              <input type='submit' value='Submit' onClick='submitMessage()'>
-            </div>
-        </form><br>
-        <form action='/get' target='hidden-form'>
-            <p>Flow Threshold : (current value %flowThres%)</p>
-            <div class='inputField'>
-              <input type='text' name='flowThres'>
-              <input type='submit' value='Submit' onClick='submitMessage()'>
-            </div>
-        </form><br>
-        <iframe style="display: none" name="hidden-form"></iframe>
-    </div>
-
-    <script>
-        var gateway = `ws://${window.location.hostname}:81/`;
-        var websocket;
-        window.addEventListener('load', onLoad);
-
-        function initWebSocket() {
-            console.log('Trying to open a WebSocket connection');
-            websocket = new WebSocket(gateway);
-            websocket.onopen = onOpen;
-            websocket.onclose = onClose;
-            websocket.onmessage = function (event) {
-                processCommand(event);
-            };
-        }
-
-        function onOpen(event) {
-            console.log('Connection opened');
-        }
-
-        function onClose(event) {
-            console.log('Connection closed');
-            setTimeout(initWebSocket, 2000);
-        }
-
-        function processCommand(event) {
-            var obj = JSON.parse(event.data);
-            document.getElementById('temp').innerHTML = obj.temp;
-            document.getElementById('hum').innerHTML = obj.hum;
-            document.getElementById('flow').innerHTML = obj.flow;
-            document.getElementById('uvstats').innerHTML = obj.uvstats;
-            document.getElementById('fanstats').innerHTML = obj.fanstats;
-            document.getElementById('tcstats').innerHTML = obj.tcstats;
-
-            console.log(obj.temp);
-            console.log(obj.hum);
-            console.log(obj.flow);
-            console.log(obj.uvstats);
-            console.log(obj.fanstats);
-            console.log(obj.tcstats);
-        }
-
-        function onLoad(event) {
-            initWebSocket();
-        }
-
-        function submitMessage(){
-          alert("Saved value to ESP SPIFFS");
-          setTimeout(function(){document.location.reload(false);},500);
-        }
-    </script>
-</body>
-
-</html>
-)rawliteral";
-
 // Functions declaration
 void StartSPIFFS();
 int StartWiFi(const char *ssid, const char *password);
@@ -243,9 +60,6 @@ void IRAM_ATTR pulseCounter();
 void readDHTSensors();
 void readFlowSensors();
 String processor(const String &var);
-// void handleWebSocketMessage(void *arg, uint8_t *data, size_t len);
-// void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len);
-// void initWebSocket();
 void webSocketEvent(byte num, WStype_t type, uint8_t *payload, size_t length);
 void notFound(AsyncWebServerRequest *request);
 void update_webpage();
@@ -277,13 +91,50 @@ void setup()
   }
 
   dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print(F("Sensor Type: "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:  "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:   "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:   "));
+  Serial.print(sensor.max_value);
+  Serial.println(F("°C"));
+  Serial.print(F("Min Value:   "));
+  Serial.print(sensor.min_value);
+  Serial.println(F("°C"));
+  Serial.print(F("Resolution:  "));
+  Serial.print(sensor.resolution);
+  Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print(F("Sensor Type: "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:  "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:   "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:   "));
+  Serial.print(sensor.max_value);
+  Serial.println(F("%"));
+  Serial.print(F("Min Value:   "));
+  Serial.print(sensor.min_value);
+  Serial.println(F("%"));
+  Serial.print(F("Resolution:  "));
+  Serial.print(sensor.resolution);
+  Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
   // StartSPIFFS();
   StartWiFi(ssid, password);
   attachInterrupt(digitalPinToInterrupt(FLOW_PIN), pulseCounter, FALLING);
-  // server.on("/", []()
-  //           { server.send(200, "text\html", web); });
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send_P(200, "text/html", index_html, processor); });
+            { request->send(SPIFFS, "/index.html", String(), false, processor); });
 
   server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
             {
@@ -399,10 +250,13 @@ void IRAM_ATTR pulseCounter()
 
 void readDHTSensors()
 {
-  humid = dht.readHumidity();
-  temp = dht.readTemperature();
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  dht.humidity().getEvent(&event);
+  humid = event.relative_humidity;
+  temp = event.temperature;
 
-  if (isnan(humid) || isnan(temp))
+  if (isnan(event.relative_humidity) || isnan(event.temperature))
   {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
@@ -465,7 +319,7 @@ void update_webpage()
 
 String readFile(fs::FS &fs, const char *path)
 {
-  Serial.printf("Readinf file: %s\r\n", path);
+  // Serial.printf("Readinf file: %s\r\n", path);
   File file = fs.open(path, "r");
 
   if (!file || file.isDirectory())
@@ -473,13 +327,13 @@ String readFile(fs::FS &fs, const char *path)
     Serial.println(F("- empty file or failed to open file"));
     return String();
   }
-  Serial.println(F("- read from file:"));
+  // Serial.println(F("- read from file:"));
   String fileContent;
   while (file.available())
   {
     fileContent += String((char)file.read());
   }
-  Serial.println(fileContent);
+  // Serial.println(fileContent);
   return fileContent;
 }
 
