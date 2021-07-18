3ccb6c2cd19fb2ee4c92c8e27e098c4443dcd730
aryawiratama18 <NAwiratama@gmail.com>
2021-07-18 08:51:09 +0700

    Add input threshold field

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
 #include "SPIFFS.h"
 #include <SPI.h>
 #include <AsyncTCP.h>
-// #include <ESPAsyncWebServer.h>
-#include <WebServer.h>
+#include <ESPAsyncWebServer.h>
+// #include <WebServer.h>
 #include <ArduinoJson.h>
 #include <WebSocketsServer.h>
 #include <WiFiClientSecure.h>
@@ -15,9 +15,9 @@
 String version = "v1.0";
 String site_width = "1023";
 WiFiClient client;
-WebServer server(80);
+// WebServer server(80);
+AsyncWebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
-// AsyncWebServer server(80);
 // AsyncWebSocket ws("/ws");
 
 // DHT 22
@@ -46,11 +46,180 @@ float flowRate;
 unsigned int flowMiliLitres;
 unsigned int totalMiliLitres;
 String uvstatus, fanstatus, thermocoolerstatus;
-String flowThres = "30";
-String humidThres, tempThres;
+String flowThres = "10";
+String humidThres = "80";
+String tempThres = "30";
 String jsonString;
-String web = "<!DOCTYPE HTML><html><head> <title>Smart APD </title> <meta name='viewport' content='width=device-width, initial-scale=1'> <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.2/css/all.css' integrity='sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr' crossorigin='anonymous'> <link rel='icon' href='data:,'> <script src='https://code.highcharts.com/highcharts.js'></script> <style> html { font-family: Arial, sans-serif; display: inline-block; text-align: center; } p { font-size: 1.2rem; } body { min-width: 310px; max-width: 800px; height: 400px; margin: 0 auto; } .topnav { overflow: hidden; background-color: #50B8B4; color: white; font-size: 1rem; } .content { padding: 20px; } .card { background-color: white; box-shadow: 2px 2px 12px 1px rgba(140, 140, 140, .5); border-radius: 5px; } .cards { max-width: 800px; margin: 10px auto; display: grid; grid-gap: 2rem; grid-template-columns: repeat(auto-fit, minmax(200px, 1fr)); } .reading { font-size: 1.4rem; } @media screen and (max-width : 320px) { .cards { max-width: 100px; margin: 10px auto; grid-template-columns: repeat(3, 1fr); } } </style></head><body> <div class='topnav'> <h1>Smart APD Webserver</h1> </div> <div class='content'> <div class='cards'> <div class='card'> <p><i class='fas fa-thermometer-half' style='color:#059e8a;'></i> TEMPERATURE</p> <p><span class='reading'><span id='temp'>%TEMPERATURE%</span> &deg;C</span></p> </div> <div class='card'> <p><i class='fas fa-tint' style='color:#00add6;'></i> HUMIDITY</p> <p><span class='reading'><span id='hum'>%HUMIDITY%</span> &percnt;</span></p> </div> <div class='card'> <p><i class='fas fa-angle-double-down' style='color:#e1e437;'></i> FLOW RATE</p> <p><span class='reading'><span id='flow'>%FLOWRATE%</span> L/min</span></p> </div> </div> <div class='cards'> <div class='card'> <p>UV STATUS</p> <p class='reading'><span id='uvstats'>%UVSTATUS%</span></p> </div> <div class='card'> <p>FAN STATUS</p> <p class='reading'><span id='fanstats'>%FANSTATUS%</span></p> </div> <div class='card'> <p>COOLER STATUS</p> <p class='reading'><span id='tcstats'>%THERMOCOOLERSTATUS%</span></p> </div> </div> </div> <script> var gateway = `ws://${window.location.hostname}:81/`; var websocket; window.addEventListener('load', onLoad); function initWebSocket() { console.log('Trying to open a WebSocket connection'); websocket = new WebSocket(gateway); websocket.onopen = onOpen; websocket.onclose = onClose; websocket.onmessage = function (event) { processCommand(event); }; } function onOpen(event) { console.log('Connection opened'); } function onClose(event) { console.log('Connection closed'); setTimeout(initWebSocket, 2000); } function processCommand(event) { var obj = JSON.parse(event.data); document.getElementById('temp').innerHTML = obj.temp; document.getElementById('hum').innerHTML = obj.hum; document.getElementById('flow').innerHTML = obj.flow; document.getElementById('uvstats').innerHTML = obj.uvstats; document.getElementById('fanstats').innerHTML = obj.fanstats; document.getElementById('tcstats').innerHTML = obj.tcstats; console.log(obj.temp); console.log(obj.hum); console.log(obj.flow); console.log(obj.uvstats); console.log(obj.fanstats); console.log(obj.tcstats); } function onLoad(event) { initWebSocket(); } </script></body></html>";
+const char *param1 = "tempThres";
+const char *param2 = "humidThres";
+const char *param3 = "flowThres";
 
+const char index_html[] PROGMEM = R"rawliteral(
+  <!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Smart APD </title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.2/css/all.css'
+        integrity='sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr' crossorigin='anonymous'>
+    <link rel='icon' href='data:,'>
+    <script src='https://code.highcharts.com/highcharts.js'></script>
+    <style>
+        html {
+            font-family: Arial, sans-serif;
+            display: inline-block;
+            text-align: center;
+        }
+
+        p {
+            font-size: 1.2rem;
+        }
+
+        body {
+            min-width: 310px;
+            max-width: 800px;
+            height: 400px;
+            margin: 0 auto;
+        }
+
+        .topnav {
+            overflow: hidden;
+            background-color: #50B8B4;
+            color: white;
+            font-size: 1rem;
+        }
+
+        .content {
+            padding: 20px;
+        }
+
+        .card {
+            background-color: white;
+            box-shadow: 2px 2px 12px 1px rgba(140, 140, 140, .5);
+            border-radius: 5px;
+        }
+
+        .cards {
+            max-width: 800px;
+            margin: 10px auto;
+            display: grid;
+            grid-gap: 2rem;
+            grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+        }
+
+        .reading {
+            font-size: 1.4rem;
+        }
+
+        @media screen and (max-width : 320px) {
+            .cards {
+                max-width: 100px;
+                margin: 10px auto;
+                grid-template-columns: repeat(3, 1fr);
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <div class='topnav'>
+        <h1>Smart APD Webserver</h1>
+    </div>
+    <div class='content'>
+        <div class='cards'>
+            <div class='card'>
+                <p><i class='fas fa-thermometer-half' style='color:#059e8a;'></i> TEMPERATURE</p>
+                <p><span class='reading'><span id='temp'>%TEMPERATURE%</span> &deg;C</span></p>
+            </div>
+            <div class='card'>
+                <p><i class='fas fa-tint' style='color:#00add6;'></i> HUMIDITY</p>
+                <p><span class='reading'><span id='hum'>%HUMIDITY%</span> &percnt;</span></p>
+            </div>
+            <div class='card'>
+                <p><i class='fas fa-angle-double-down' style='color:#e1e437;'></i> FLOW RATE</p>
+                <p><span class='reading'><span id='flow'>%FLOWRATE%</span> L/min</span></p>
+            </div>
+        </div>
+
+        <div class='cards'>
+            <div class='card'>
+                <p>UV STATUS</p>
+                <p class='reading'><span id='uvstats'>%UVSTATUS%</span></p>
+            </div>
+            <div class='card'>
+                <p>FAN STATUS</p>
+                <p class='reading'><span id='fanstats'>%FANSTATUS%</span></p>
+            </div>
+            <div class='card'>
+                <p>COOLER STATUS</p>
+                <p class='reading'><span id='tcstats'>%THERMOCOOLERSTATUS%</span></p>
+            </div>
+        </div>
+
+        <form action='/get'>
+            Temperature Threshold : <input type='text' name='tempThres'>
+            <input type='submit' value='Submit'>
+        </form><br>
+        <form action='/get'>
+            Humidity Threshold : <input type='text' name='humidThres'>
+            <input type='submit' value='Submit'>
+        </form><br>
+        <form action='/get'>
+            Flow Threshold : <input type='text' name='flowThres'>
+            <input type='submit' value='Submit'>
+        </form><br>
+    </div>
+
+    <script>
+        var gateway = `ws://${window.location.hostname}:81/`;
+        var websocket;
+        window.addEventListener('load', onLoad);
+
+        function initWebSocket() {
+            console.log('Trying to open a WebSocket connection');
+            websocket = new WebSocket(gateway);
+            websocket.onopen = onOpen;
+            websocket.onclose = onClose;
+            websocket.onmessage = function (event) {
+                processCommand(event);
+            };
+        }
+
+        function onOpen(event) {
+            console.log('Connection opened');
+        }
+
+        function onClose(event) {
+            console.log('Connection closed');
+            setTimeout(initWebSocket, 2000);
+        }
+
+        function processCommand(event) {
+            var obj = JSON.parse(event.data);
+            document.getElementById('temp').innerHTML = obj.temp;
+            document.getElementById('hum').innerHTML = obj.hum;
+            document.getElementById('flow').innerHTML = obj.flow;
+            document.getElementById('uvstats').innerHTML = obj.uvstats;
+            document.getElementById('fanstats').innerHTML = obj.fanstats;
+            document.getElementById('tcstats').innerHTML = obj.tcstats;
+
+            console.log(obj.temp);
+            console.log(obj.hum);
+            console.log(obj.flow);
+            console.log(obj.uvstats);
+            console.log(obj.fanstats);
+            console.log(obj.tcstats);
+        }
+
+        function onLoad(event) {
+            initWebSocket();
+        }
+    </script>
+</body>
+
+</html>
+)rawliteral";
 // Functions declaration
 void StartSPIFFS();
 int StartWiFi(const char *ssid, const char *password);
@@ -62,6 +231,7 @@ void readFlowSensors();
 // void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len);
 // void initWebSocket();
 void webSocketEvent(byte num, WStype_t type, uint8_t *payload, size_t length);
+void notFound(AsyncWebServerRequest *request);
 void update_webpage();
 
 void setup()
@@ -92,13 +262,35 @@ void setup()
   // StartSPIFFS();
   StartWiFi(ssid, password);
   attachInterrupt(digitalPinToInterrupt(FLOW_PIN), pulseCounter, FALLING);
+  // server.on("/", []()
+  //           { server.send(200, "text\html", web); });
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send_P(200, "text/html", index_html); });
 
-  // initWebSocket();
-  // Handle Web Server
-  // server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-  //           { request->send(SPIFFS, "/index.html", String(), false, processor); });
-  server.on("/", []()
-            { server.send(200, "text\html", web); });
+  server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              String inputParam, inputMessage;
+              if (request->hasParam(param1))
+              {
+                inputParam = param1;
+                inputMessage = request->getParam(param1)->value();
+                tempThres = request->getParam(param1)->value();
+              }
+              else if (request->hasParam(param2))
+              {
+                inputParam = param2;
+                inputMessage = request->getParam(param2)->value();
+                humidThres = request->getParam(param2)->value();
+              }
+              else if (request->hasParam(param3))
+              {
+                inputParam = param3;
+                inputMessage = request->getParam(param3)->value();
+                flowThres = request->getParam(param3)->value();
+              }
+              request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" + inputParam + ") with value: " + inputMessage + "<br><a href=\"/\">Return to Home Page</a>");
+            });
+  server.onNotFound(notFound);
   server.begin();
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
@@ -106,7 +298,7 @@ void setup()
 
 void loop()
 {
-  server.handleClient();
+  // server.handleClient();
   webSocket.loop();
   if ((millis() - previousMillis) > interval)
   {
@@ -116,6 +308,10 @@ void loop()
     previousMillis = millis();
     flowMiliLitres = (flowRate / 60) * 1000;
     totalMiliLitres += flowMiliLitres;
+
+    Serial.println(tempThres);
+    Serial.println(humidThres);
+    Serial.println(flowThres);
 
     Serial.print(F("Flow rate: "));
     Serial.print(int(flowRate)); // Print the integer part of the variable
@@ -234,40 +430,15 @@ void webSocketEvent(byte num, WStype_t type, uint8_t *payload, size_t length)
     Serial.print(F("WS Type: "));
     Serial.print(type);
     Serial.println(F(" CONNECTED"));
-    // if (digitalRead(UV) == LOW)
-    // {
-    //   uvstatus = "ON";
-    //   update_webpage();
-    // }
-    // else
-    // {
-    //   uvstatus = "OFF";
-    //   update_webpage();
-    // }
-    // if (digitalRead(KIPAS) == LOW)
-    // {
-    //   fanstatus = "ON";
-    //   update_webpage();
-    // }
-    // else
-    // {
-    //   fanstatus = "OFF";
-    //   update_webpage();
-    // }
-    // if (digitalRead(PELTIER) == LOW)
-    // {
-    //   thermocoolerstatus = "ON";
-    //   update_webpage();
-    // }
-    // else
-    // {
-    //   thermocoolerstatus = "OFF";
-    //   update_webpage();
-    // }
     break;
   case WStype_TEXT:
     break;
   }
+}
+
+void notFound(AsyncWebServerRequest *request)
+{
+  request->send(404, "text/plain", "Not found");
 }
 
 void update_webpage()
